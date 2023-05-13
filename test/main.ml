63851ffe497677bfa376ac784dbb8654792d4faf
Aidan O'Connor <ao274@cornell.edu>
2023-05-13 12:43:12 -0400

    Added more territory tests and added opendoc capabilities

--- test/main.ml
+++ test/main.ml
@@ -2,7 +2,32 @@ open OUnit2
 open Game
 open Game__Board
 
-(* Helper functions cmp_set_like_lists, pp_string, pp_list taken from A2 *)
+(** TEST PLAN:
+We tested all of our functions that don't have any user input using OUnit. 
+All of our functions in the board module were able to be tested with OUnit. 
+We tested each function in board at least once, and some of the larger functions
+were tested more than once. It became a little more difficult to test the functions in
+the game module since a lot of them require user input at some point in the execution. 
+We found that it was significantly easier to test these functions that had terminal ouput 
+through actual gameplay. We constructed our tests mainly through random testing. 
+We gave the functions random game states and made sure the output was what we wanted. 
+Most of the tests were glass box tests since the implementer was usually the one writing 
+the tests. However, for our large game functions we made sure that people who didn't 
+implement the functions still ensured of correctness. The smaller functions in the game and 
+board compilation units were automatically tested with OUnit and the larger ones we made 
+sure to cover every line possible when playing through our game. Functions like draft 
+where there are many conditionals we ran through many times to ensure of correctness. A 
+large portion of our debugging was done through this iterative process. We spent quite a few
+hours ensuring each line of code was executed. We think that our test suite and gameplay 
+testing approach demonstrate correctness of the system because we were quite thorough in 
+the process. We wrote down all conditionals that needed to be executed in all of our game 
+functions and checked off when each branch of the conditionals were executed. We spent over 
+10 hours on this testing process alone and we feel very confident in the correctness of 
+our system thanks to our process. *)
+
+
+(* Helper functions cmp_set_like_lists, pp_string, pp_list taken from A2.
+   pp_int, and pp_tuple were made for our own usage*)
 
 (** [cmp_set_like_lists lst1 lst2] compares two lists to see whether they are
     equivalent set-like lists. That means checking two things. First, they must
@@ -18,10 +43,10 @@ let cmp_set_like_lists lst1 lst2 =
 
 (** [pp_string s] pretty-prints string [s]. *)
 let pp_string s = "\"" ^ s ^ "\""
+(** [pp_int i] pretty-prints int [i]. *)
 let pp_int i = string_of_int i
+(** [pp_tuple t] pretty-prints tuple [t]. *)
 let pp_tuple t = "(\"" ^ fst t ^ "\",\"" ^ snd t ^ "\")"
-
-let pp_int i = string_of_int i
 
 (** [pp_list pp_elt lst] pretty-prints list [lst], using [pp_elt] to
     pretty-print each element of [lst]. *)
@@ -86,7 +111,6 @@ let ibc = cornell_yojson |> territories_from_file
 let duffield = cornell_yojson |> territories_from_file
 |> get_territory_from_string "Duffield"
 
-
 let add_armies_to_territory_test (name : string) (t : territory)
     (num_troops : int) (expected : int) : test =
   name >:: fun _ ->
@@ -111,6 +135,7 @@ let get_neighbors_test (name : string) (t : territory) (expected : string list)
   name >:: fun _ ->
   assert_equal ~cmp:cmp_set_like_lists ~printer:(pp_list pp_string) expected
     (get_neighbors t)
+
 let get_name_test (name : string) (t : territory) (expected_output : string) : test =
   name >:: fun _ -> 
     assert_equal expected_output (get_territory_name t)
@@ -130,7 +155,58 @@ let board_tests =
         "Ontario";
         "Alberta";
       ];
-
+    get_territories_from_continent_test "Check territories of South America"
+      "South America" territory_yojson
+      ["Argentina"; 
+        "Brazil"; 
+        "Venezuela"; 
+        "Peru"
+      ];
+    get_territories_from_continent_test "Check territories of Africa"
+      "Africa" territory_yojson
+      [
+        "Congo"; 
+        "East Africa"; 
+        "Egypt"; 
+        "Madagascar"; 
+        "North Africa"; 
+        "South Africa"
+      ];
+    get_territories_from_continent_test "Check territories of Europe"
+      "Europe" territory_yojson
+      [
+        "Great Britain"; 
+        "Iceland"; 
+        "Northern Europe"; 
+        "Scandanavia"; 
+        "Southern Europe"; 
+        "Ukraine"; 
+        "Western Europe"
+      ]; 
+    get_territories_from_continent_test "Check territories of Australia"
+      "Australia" territory_yojson
+      [
+        "Eastern Australia"; 
+        "New Guinea"; 
+        "Indonesia"; 
+        "Western Australia"
+      ];
+    get_territories_from_continent_test "Check territories of Asia"
+      "Asia" territory_yojson
+      [
+        "Afghanistan"; 
+        "China"; 
+        "India"; 
+        "Irkutsk"; 
+        "Japan"; 
+        "Kamchatka"; 
+        "Middle East"; 
+        "Mongolia"; 
+        "Siam"; 
+        "Siberia"; 
+        "Ural"; 
+        "Yakutsk"
+      ];        
     get_territories_from_continent_test "Check territories of North Campus" 
       "North Campus" cornell_yojson
       [
@@ -145,7 +221,26 @@ let board_tests =
         "Morrison";
         "CKB"
       ];
-
+    get_territories_from_continent_test "Check territories of Eng Quad"
+    "Engineering Quad" cornell_yojson
+    [
+      "Statler"; 
+      "Duffield"; 
+      "Carpenter"; 
+      "Construction"; 
+      "Rhodes"; 
+      "Snee"
+    ];
+    get_territories_from_continent_test "Check territories of Arts Quad"
+    "Arts Quad" cornell_yojson
+    [
+      "Ho Plaza"; 
+      "Olin"; 
+      "Goldwin Smith"; 
+      "Johnson"; 
+      "McGraw"; 
+      "Uris"
+    ];
     get_territories_from_continent_test "Check territories of College Town"
     "College Town" cornell_yojson
     [
@@ -157,7 +252,16 @@ let board_tests =
       "Level B";
       "Loco"
     ];
-
+    get_territories_from_continent_test "Check territories of West Campus"
+    "West Campus" cornell_yojson
+    [
+      "Baker Tower"; 
+      "Noyes Rec"; 
+      "Keaton"; 
+      "Bethe"; 
+      "Rose"; 
+      "Cook"
+    ];
     get_territory_from_string_test "Search for Venezuela" "Venezuela"
       territory_yojson "Venezuela";
     get_territory_from_string_test "Search for Greenland" "Greenland"
@@ -180,31 +284,55 @@ let board_tests =
         "Ural";
         "Yakutsk";
       ];
+    territories_list_test "Check territories of North America as String List"
+      territory_yojson "North America"
+      [
+        "Alaska"; 
+        "Alberta"; 
+        "Central America"; 
+        "Eastern US"; 
+        "Greenland"; 
+        "Northwest Territory"; 
+        "Ontario"; 
+        "Quebec"; 
+        "Western US"
+      ];  
+    territories_list_test "Check territories of Arts Quad as String List"
+      cornell_yojson "Arts Quad"
+      [
+        "Ho Plaza"; 
+        "Olin"; 
+        "Goldwin Smith"; 
+        "Johnson"; 
+        "McGraw"; 
+        "Uris"
+      ];
+    territories_list_test "Check territories of Engineering Quad as String List"
+      cornell_yojson "Engineering Quad"
+      [
+        "Statler"; 
+        "Duffield"; 
+        "Carpenter"; 
+        "Construction"; 
+        "Rhodes"; 
+        "Snee"
+      ];   
     add_armies_to_territory_test "Add 2 troops to Greenland" greenland 2 2;
-    add_armies_to_territory_test "Add 1 troop to Greenland" greenland 1 1;
-    get_territory_numtroops_test "Get num troops from Greenland" greenland 2;
     get_territory_numtroops_test "Get num troops from South Africa" south_africa
       0;
     add_armies_to_territory_test "Add troops to South Africa" south_africa 0 0;
-    (* get_player_number_test "Get Greenland's owner (-1)" greenland ~-1; *)
     set_territory_owner_test "Set Greenlands owner to 2" greenland 2 2;
-    set_territory_owner_test "Set Duffield's owner to 1" duffield 1 1;
-    (* get_player_number_test "Get Greenland's owner (2)" greenland ~-1; *)
+    set_territory_owner_test "Set Duffield's owner to 1" duffield 1 1; 
     get_neighbors_test "Get South Africa's neighbors" south_africa
       [ "Madagascar"; "Congo"; "East Africa" ];
     get_neighbors_test "Get Greenland neighbors" greenland
       [ "Iceland"; "Northwest Territory"; "Ontario"; "Quebec" ];
-
     get_neighbors_test "Get IBC's neighbors" ibc
       [ "7/11"; "Snee"; "Construction"; "CTB"];
-    
     get_neighbors_test "Get Duffield's neighbors" duffield
-      ["Carpenter"; "Statler"; "Construction"; "Rhodes"];
-
+      ["Carpenter"; "Statler"; "Construction"; "Rhodes"]; 
     get_name_test "Get Greenland's name" greenland "Greenland";
-
     get_name_test "Get Duffield's name" duffield "Duffield";
-
     get_name_test "Get IBC's name" ibc "IBC"
   ]
 
@@ -308,8 +436,6 @@ let tad =
       "Arts Quad")
     0 d1  
 
-      
-
 let player_test name expected_output p =
   name >:: fun _ ->
   assert_equal true (cmp_set_like_lists expected_output (List.map get_territory_name (Game.get_territories p)))
@@ -323,61 +449,6 @@ let g3 = init_state [p3;p4;p1;p2] d1 territory_yojson
 let g4 = init_state [p_all] d1 territory_yojson
 
 let g5 = init_state [p_all;p_all;p_all;p_all] d1 territory_yojson
-
-let capture_test_with_strings
-(name : string)
-(state : Game.t)
-(t1 : string)
-(t2 : string)
-(i : int)
-(expected_output : string list) : test =
-name >:: fun _ ->
-  let x = (capture state 
-  (get_territory_from_string t1 (territories_from_file  territory_yojson))
-  (get_territory_from_string t2 (territories_from_file  territory_yojson))) in
-  assert_equal true (cmp_set_like_lists expected_output 
-  (List.map get_territory_name 
-  (Game.get_territories 
-  (List.nth (Game.get_players x) i))))
-
-let capture_territory_troops_test 
-(name : string)
-(state : Game.t)
-(t1 : Game__Board.territory)
-(t2 : Game__Board.territory)
-(i : int)
-(expected_output : int list) : test =
-name >:: fun _ ->
-  let new_s = capture state t1 t2 in
-  let list = (List.map Game__Board.get_territory_numtroops (Game.get_territories (List.nth (Game.get_players new_s) i))) in 
-  assert_equal ~printer:(pp_list pp_int) expected_output list
-
-let capture_test_with_ter
-(name : string)
-(state : Game.t)
-(t1 : Game__Board.territory)
-(t2 : Game__Board.territory)
-(i : int)
-(expected_output : string list) : test =
-name >:: fun _ ->
-  let new_s = capture state t1 t2 in 
-  let list = List.map Game__Board.get_territory_name (Game.get_territories (List.nth (Game.get_players new_s) i)) in
-  assert_equal ~printer:(pp_list pp_string) expected_output list
-
-let battle_decision_test 
-(name : string)
-(state : Game.t)
-(d1 : int)
-(d2 : int)
-(t1 : Game__Board.territory)
-(t2 : Game__Board.territory)
-(i : int)
-(expected_output : int list) : test =
-name >:: fun _ ->
-  let new_s = Game.battle_decision state d1 d2 t1 t2 in
-  let player = List.nth (Game.get_players new_s) i in
-  let list = List.map Game__Board.get_territory_numtroops (Game.get_territories player) in 
-  assert_equal ~printer:(pp_list pp_int) expected_output list
 
 let elimination_test (name : string) (state : Game.t) (player : Game.player)
     (expected_output : string list) : test =
@@ -395,17 +466,6 @@ let update_list_test (name : string) (lst : Game__Board.territory list)
   let list = List.map Game__Board.get_territory_numtroops out in
   assert_equal ~printer:(pp_list pp_int) expected_output list
 
-let fortify_test (name : string) (state : Game.t) (expected_output : int list) :
-    test =
-  name >:: fun _ ->
-  let new_s = fortify state in
-  let current_player = List.hd (Game.get_players new_s) in
-  let list =
-    List.map Game__Board.get_territory_numtroops
-      (Game.get_territories current_player)
-  in
-  assert_equal ~printer:(pp_list pp_int) expected_output list
-
 let finished_game_test 
   (name : string)
   (state : Game.t)
@@ -413,11 +473,9 @@ let finished_game_test
   name >:: fun _ ->
   assert_equal expected_output (finished_game state)
 
-
 let game_tests =
   [
     deck_test "Initial" d1 [("Cavalry","Alaska"); ("Infantry","Alberta"); ("Artillery","Central America"); ("Cavalry","Eastern US"); ("Infantry","Greenland"); ("Artillery","Northwest Territory"); ("Cavalry","Ontario"); ("Infantry","Quebec"); ("Artillery","Western US"); ("Cavalry","Argentina"); ("Infantry","Brazil"); ("Artillery","Venezuela"); ("Cavalry","Peru"); ("Infantry","Congo"); ("Artillery","Alaska"); ("Cavalry","Alberta"); ("Infantry","Central America"); ("Artillery","Eastern US"); ("Cavalry","Greenland"); ("Infantry","Northwest Territory"); ("Artillery","Ontario"); ("Cavalry","Quebec"); ("Infantry","Western US"); ("Artillery","Argentina"); ("Cavalry","Brazil"); ("Infantry","Venezuela"); ("Artillery","Peru"); ("Cavalry","Congo"); ("Infantry","Alaska"); ("Artillery","Alberta"); ("Cavalry","Central America"); ("Infantry","Eastern US"); ("Artillery","Greenland"); ("Cavalry","Northwest Territory"); ("Infantry","Ontario"); ("Artillery","Quebec"); ("Cavalry","Western US"); ("Infantry","Argentina"); ("Artillery","Brazil"); ("Cavalry","Venezuela"); ("Infantry","Peru"); ("Artillery","Congo")];
-    
     deck_test "Cornell" d2 [("Cavalry","RPCC"); ("Infantry","Low-Rises"); ("Artillery","Donlon"); ("Cavalry","Appel"); ("Infantry","Jameson"); ("Artillery","Ganedago"); ("Cavalry","Dickson"); ("Infantry","Helen Newman"); ("Artillery","Morrison"); ("Cavalry","CKB"); ("Infantry","Statler"); ("Artillery","RPCC"); ("Cavalry","Low-Rises"); ("Infantry","Donlon"); ("Artillery","Appel"); ("Cavalry","Jameson"); ("Infantry","Ganedago"); ("Artillery","Dickson"); ("Cavalry","Helen Newman"); ("Infantry","Morrison"); ("Artillery","CKB"); ("Cavalry","Statler"); ("Infantry","RPCC"); ("Artillery","Low-Rises"); ("Cavalry","Donlon"); ("Infantry","Appel"); ("Artillery","Jameson"); ("Cavalry","Ganedago"); ("Infantry","Dickson"); ("Artillery","Helen Newman"); ("Cavalry","Morrison"); ("Infantry","CKB"); ("Artillery","Statler")];
     player_test "North America" [
       "Alaska";
@@ -430,32 +488,27 @@ let game_tests =
       "Ontario";
       "Alberta";
     ] p1; 
-
     player_test "South America" [
       "Argentina";
       "Brazil";
       "Venezuela";
       "Peru"
     ] p2;
-
     player_test "Asia" [
       "China";
       "India"
     ] p3;
-
     player_test "Europe" [
       "Scandanavia";
       "Iceland";
       "Great Britain"
     ] p4;
-
     player_test "Australia" [
       "Eastern Australia";
       "New Guinea";
       "Western Australia";
       "Indonesia"
     ] p5;
-
     player_test "Africa" [
       "Congo";
       "East Africa";
@@ -464,7 +517,6 @@ let game_tests =
       "North Africa";
       "South Africa"
     ] p6;
-
     player_test "North Campus" [
       "RPCC";
       "Low-Rises";
@@ -477,7 +529,6 @@ let game_tests =
       "Morrison";
       "CKB"
     ] rowan;
-
     player_test "Engineering Quad" [
       "Statler";
       "Duffield";
@@ -486,7 +537,6 @@ let game_tests =
       "Rhodes";
       "Snee"
     ] aidan;
-
     player_test "College Town" [
       "IBC";
       "7/11";
@@ -496,7 +546,6 @@ let game_tests =
       "Level B";
       "Loco"
     ] teresa;
-
     player_test "Arts Quad" [
       "Ho Plaza";
       "Olin";
@@ -505,7 +554,6 @@ let game_tests =
       "McGraw";
       "Uris"
     ] tad;
-
     player_test "West Campus" [
       "Baker Tower";
       "Noyes Rec";
@@ -514,73 +562,17 @@ let game_tests =
       "Rose";
       "Cook"
     ] nigel;
-
-    (* capture_test_with_strings "Capturing" g1 "Central America" "Venezuela" 0[
-      "Alaska";
-      "Northwest Territory";
-      "Greenland";
-      "Quebec";
-      "Eastern US";
-      "Western US";
-      "Central America";
-      "Ontario";
-      "Alberta";
-      "Venezuela";
-    ];
-    capture_test_with_strings "Captured" g1 "Central America" "Venezuela" 1[
-      "Argentina";
-      "Peru";
-      "Brazil"
-    ]; 
-
-    capture_test_with_ter "Great Britain captures China" g2 gb china 0 [
-      "China";
-      "Scandanavia";
-      "Iceland";
-      "Great Britain"
-    ];
-
-    capture_test_with_ter "China captured" g2 gb china 1 [
-      "India"
-    ]; *)
-
-
-    (* capture_territory_troops_test "capturing china by GB" g2 gb china 0 [5; 3; 5; 4]; 
-    capture_territory_troops_test "captured China" g2 gb china 1 [4];
-
-    battle_decision_test "3 vs 1" g2 3 1 gb china 1 [1; 4];
-    battle_decision_test "3 vs 1" g2 3 1 gb china 0 [3; 5; 7];
-    battle_decision_test "3 vs 1" g2 3 1 gb china 1 [1; 4];
-    
-    battle_decision_test "3 vs 1 2nd try" g2 3 1 gb china 1 [1;4];
-    battle_decision_test "3 vs 1 3rd" g2 3 1 gb china 0 [3; 5; 6]; *)
-
-
-
-    elimination_test "Elimination test" g1 p2 ["Bob"];
-
-    elimination_test "Eliminating with multiple players" g2 p1 ["Matt"; "Joe"; "Dave"];
-
-    elimination_test "Eliminating with only one player" g4 p_all [];
-
-    elimination_test "Eliminating the middle element" g2 p3 ["Matt"; "Bob"; "Dave"];
-
-
-    (* update_list_test "updating Iceland" t1 iceland 5 [3;10;7];
-
-    update_list_test "updating China" t2 china 12 [14;4]; *)
-
-    (* fortify_test "Scandanavia to Iceland" g2 [1; 7; 7]; 
-
-    fortify_test "China to India" g3 [1; 5]; *)
-
+    elimination_test "elimination test" g1 p2 ["Bob"];
+    elimination_test "eliminating with multiple players" g2 p1 ["Matt"; "Joe"; "Dave"];
+    elimination_test "eliminating with only one player" g4 p_all [];
+    elimination_test "eliminating the middle element" g2 p3 ["Matt"; "Bob"; "Dave"];
+    update_list_test "updating Iceland" t1 iceland 5 [3;10;7];
+    update_list_test "updating China" t2 china 12 [14;4];
     finished_game_test "state 1" g1 false;
     finished_game_test "state 2" g2 false;
     finished_game_test "state 3" g3 false;
     finished_game_test "finished state" g4 true;
     finished_game_test "multiple winnings" g5 true
-
-
 ]
 
 let suite = "test suite for risk" >::: List.flatten [ board_tests; game_tests ]
